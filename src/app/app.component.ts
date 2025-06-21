b24ffd1978f2b8558c8ee13e28e99e204fc7d380
Yan Lobo <yanvlobo05@gmail.com>
2025-06-21 06:18:46 -0300

    feat: estrutura inicial do app e funcionalidades básicas de tarefas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'organizador-tarefas';
+  novaTarefa = '';
+  tarefas: { nome: string; feita: boolean }[] = [];
+
+  adicionarTarefa() {
+    if (this.novaTarefa.trim()) {
+      this.tarefas.push({ nome: this.novaTarefa.trim(), feita: false });
+      this.novaTarefa = '';
+    }
+  }
+
+  removerTarefa(index: number) {
+    this.tarefas.splice(index, 1);
+  }
 }
